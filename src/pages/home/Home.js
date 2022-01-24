22b1417a31b306bb7ea4e2fc03a502606317eaa7
dngst <dngst@protonmail.com>
2022-01-24 20:37:56 +0300

    Move to pages directory

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from "wouter";
+import Button from "../../components/button/Button";
+import './Home.css';
+
+const Home = () => {
+    document.title = 'Bright Events - Create & Manage Events';
+    return (
+        <div className="index">
+            <h1>plan your next event</h1>
+            <Button><Link to="#">get started</Link></Button>
+        </div>
+    );
+}
+
+export default Home;
