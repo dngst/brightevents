315e57bc350943de74474eae55a303109da7990b
dngst <dngst@protonmail.com>
2022-01-25 17:27:08 +0300

    Set max width

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-  width: 21.1rem;
-  height: 10rem;
+  width: 100%;
+  height: 170px;
   background-color: #f78f1e;
   margin: 0 0 1.5%;
 `
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -3,16 +3,19 @@ import styled from "styled-components";
 const Input = styled.input.attrs(props => ({
   type: 'text'
 }))`
-  border-radius: 3px;
   border: 1px solid #f78f1e;
   display: block;
-  padding: 0.5rem;
-  width: 20rem;
+  height: 2rem;
+  width: 100%;
   margin: 0 0 1em;
   text-transform: capitalize;
   border-radius: 0;
   ::placeholder {
     color: #f78f1e;
+  }
+  &:focus {
+    outline: none;
+    border-color: #ffffff;
   }
 `
 
