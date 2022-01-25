cca2878190dfc61b179a618a6f03b3043b55e43f
dngst <dngst@protonmail.com>
2022-01-25 04:31:03 +0300

    Add card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 21.1rem;
+  height: 10rem;
+  background-color: #f78f1e;
+  margin: 0 0 1.5%;
+`
+
+export default Card;
