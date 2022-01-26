67447df0b6011f3015f1290c5097381766bf128f
dngst <dngst@protonmail.com>
2022-01-26 19:14:34 +0300

    Add home container

---
+++ src/components/container/HomeContainer.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  background:  linear-gradient(rgb(247, 143, 30), rgba(0, 0, 0, 0.8)),
+  url(https://images.pexels.com/photos/2361102/pexels-photo-2361102.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260) no-repeat center center; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  min-height: 100vh;
+  margin: -0.6% -0.6% -1%;
+`
+export default HomeContainer;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,20 @@
 import { Helmet } from "react-helmet";
 import Title from "../../components/title/Title";
 import Button from "../../components/button/Button";
+import HomeContainer from "../../components/container/HomeContainer";
+
 
 const Home = () => {
     return (
-        <div className="home center">
+        <HomeContainer>
             <Helmet>
                 <title>Bright Events - Create & Manage Events</title>
             </Helmet>
-            <Title>plan your next event</Title>
+            <Title fsize="3rem" color="#ffffff" padding="15% 0 2.5%" margin="auto">
+                plan your next event
+            </Title>
             <Button as="a" href="/new" primary>get started</Button>
-        </div>
+        </HomeContainer>
     );
 }
 
