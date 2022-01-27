0fea6ccf1c66dbd87b42be5393cd0157b15bb10d
dngst <dngst@protonmail.com>
2022-01-27 11:26:36 +0300

    Remove border

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -3,13 +3,13 @@ import styled from "styled-components";
 const Input = styled.input.attrs(props => ({
   type: 'text'
 }))`
-  border: 1px solid #f78f1e;
+  border: none;
+  border-bottom: 1px solid #f78f1e;
   display: block;
   height: 2rem;
   width: 100%;
   margin: 0 0 1em;
   text-transform: capitalize;
-  border-radius: 0;
   box-sizing: border-box;
   ::placeholder {
     color: #f78f1e;
