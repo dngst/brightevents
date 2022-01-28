2c713b2be2d19e32b3bb6c4fa9779f8a21ed1913
dngst <dngst@protonmail.com>
2022-01-28 13:39:14 +0300

    Add space on top of title while scrolling

--- src/components/container/Sticky.js
+++ src/components/container/Sticky.js
@@ -4,5 +4,11 @@ const Sticky = styled.div`
   position: sticky;
   top: 0;
   background-color: #ffffff;
+  @media (max-width: 480px) {
+    h1 {
+      top: 2%;
+      position: sticky;
+    }
+  }
 `
 export default Sticky;
