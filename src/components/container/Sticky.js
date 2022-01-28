b450a517e7fae6799460a077a6fa9e8640f10a2c
dngst <dngst@protonmail.com>
2022-01-28 13:04:22 +0300

    Add sticky container

---
+++ src/components/container/Sticky.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Sticky = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: #ffffff;
+`
+export default Sticky;
--- src/pages/viewevents/ViewEvents.js
+++ src/pages/viewevents/ViewEvents.js
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 import Container from "components/container/Container";
+import Sticky from "components/container/Sticky";
 import Title from "components/title/Title";
 import Input from "components/input/Input";
 import Card from "components/card/Card";
@@ -11,8 +12,12 @@ const ViewEvents = () => {
             <Helmet>
                 <title>Events | Bright Events</title>
             </Helmet>
-            <Title>browse events</Title>
-            <Input placeholder="search events by title, location, cateogry" />
+            <Sticky>
+                <Title>browse events</Title>
+                <Input placeholder="search events by title, location, cateogry" />
+            </Sticky>
+            <Card />
+            <Card />
             <Card />
             <Card />
             <Card />
