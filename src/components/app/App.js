d06aeee0df345cf4420cbe5f107dccc720d412c8
dngst <dngst@protonmail.com>
2022-01-29 18:11:27 +0300

    Add about page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,22 @@
 import { Route, Switch } from "wouter";
 import GlobalStyle from "../globalstyle/GlobalStyle";
+import Navbar from "../nav/Navbar";
 import Home from "pages/home/Home";
 import CreateEvent from "pages/createevent/CreateEvent";
-import PageNotFound from "pages/pagenotfound/PageNotFound";
-import ResetPassword from "pages/resetpassword/ResetPassword";
-import SignUp from "pages/signup/SignUp";
 import Login from "pages/login/Login";
+import SignUp from "pages/signup/SignUp";
+import ResetPassword from "pages/resetpassword/ResetPassword";
 import ViewEvents from "pages/viewevents/ViewEvents";
 import UpdateEvent from "pages/updateevent/UpdateEvent";
+import PageNotFound from "pages/pagenotfound/PageNotFound";
+import About from "pages/about/About";
 
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Navbar />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/events" component={ViewEvents} />
@@ -22,6 +25,7 @@ const App = () => {
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
         <Route path="/resetpassword" component={ResetPassword} />
+        <Route path="/about" component={About}/>
         <Route component={PageNotFound} />
       </Switch>
     </div>
---
+++ src/pages/about/About.js
@@ -0,0 +1,19 @@
+import Container from "components/container/Container";
+import Title from "components/title/Title";
+import Paragraph from "components/paragraph/Paragraph";
+
+
+const About = () => {
+	return (
+		<Container bg>
+		<Title>about</Title>
+			<Paragraph>
+				A platform for event organizers to create and manage different
+				types of events while making them easily accessible to target
+				markets.
+			</Paragraph>
+		</Container>
+		);
+}
+
+export default About;
