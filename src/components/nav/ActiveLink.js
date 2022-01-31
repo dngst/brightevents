e5de55363946f5d3a6db7ac2dccafe3fa13ab4b0
dngst <dngst@protonmail.com>
2022-01-31 22:27:25 +0300

    Add href

--- src/components/nav/ActiveLink.js
+++ src/components/nav/ActiveLink.js
@@ -5,7 +5,7 @@ const ActiveLink = (props) => {
   const [isActive] = useRoute(props.href);
   return (
   <Link {...props}>
-    <a className={isActive ? "active" : ""}>{props.children}</a>
+    <a className={isActive ? "active" : ""} href={isActive}>{props.children}</a>
   </Link>
   );
 }
