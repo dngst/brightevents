cd570e2801a032eab538b43350eb2fe4f547043a
dngst <dngst@protonmail.com>
2022-01-31 14:11:12 +0300

    Justify text

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -20,7 +20,7 @@ const Event = () => {
             consectetur dolor labore adipisicing culpa ad do excepteur ut
             exercitation ut aliquip cupidatat voluptate labore.
             </Paragraph>
-            <Paragraph><Link to="#">#food</Link></Paragraph>
+            <Paragraph justify><Link to="#">#food</Link></Paragraph>
             <Paragraph capitalize className="card-footer">
                 nakuru | 1 feb 22 | 11:00 - 2:00
             </Paragraph>
