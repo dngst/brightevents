42858dc989a3d2eca8f2726293f31eb90d5e3be2
dngst <dngst@protonmail.com>
2022-01-31 10:45:53 +0300

    Add opacity to the background

--- src/components/container/Sticky.js
+++ src/components/container/Sticky.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Sticky = styled.div`
   position: sticky;
   top: 0;
-  background-color: #ffffff;
+  background: rgb(255, 255, 255, 0.87);
   h1 {
     padding-top: 3%;
   }
