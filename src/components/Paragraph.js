58f549863d8198962b6c94dc0569ad4cd3092b08
dngst <dngst@protonmail.com>
2022-01-31 13:50:56 +0300

    Remove font-size

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const Paragraph = styled.p`
   text-transform: ${props => props.capitalize? "capitalize" : "none"};
-  font-size: 1.1rem;
   line-height: 1.5;
   text-align: ${props => props.justify? "justify": "inherit"};
   word-break: break-all;
