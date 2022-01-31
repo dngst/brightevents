f42f4774ddf1dc46e3cdb96b248ea2d5267ed604
dngst <dngst@protonmail.com>
2022-01-31 14:27:47 +0300

    Set maximun width on mobile

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -4,11 +4,9 @@ const Container = styled.div`
   text-align: center;
   width: ${props => props.bg? "60%" : "30%"};
   margin: ${props => props.primary? "5% auto": "7% auto"};
-  @media (max-width: 768px) {
-    width: 85%;
-    margin-top: 25%;
-  }
    @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 25%;
     p {
       font-size: 0.9rem;
     }
