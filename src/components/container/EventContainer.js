c8bf0abd593972239bb4b1c5ee8b263515d0aee8
dngst <dngst@protonmail.com>
2022-01-31 23:36:05 +0300

    Remove extra padding

--- src/components/container/EventContainer.js
+++ src/components/container/EventContainer.js
@@ -2,18 +2,15 @@ import styled from "styled-components"
 import Container from "./Container";
 
 const EventContainer = styled(Container)`
-  padding: 1%;
   .event-details {
     text-align: center;
   }
   .event-btns {
     button {
-      margin: 1% auto;
+      margin: 1.2% auto;
     }
   }
   @media (max-width: 480px) {
-    width: 85%;
-    padding: 4.5%;
     h1 {
       font-size: 1.8rem;
     }
