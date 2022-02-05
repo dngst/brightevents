74bc07924929aa607e8637dcfc5ea08c18e258a5
dngst <dngst@protonmail.com>
2022-02-05 19:13:11 +0300

    Remove default word break

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -4,6 +4,6 @@ const Paragraph = styled.p`
   text-transform: ${(props) => (props.capitalize ? "capitalize" : "none")};
   line-height: 1.5;
   text-align: ${(props) => (props.justify ? "justify" : "inherit")};
-  word-break: break-all;
+  word-break: ${(props) => (props.break ? "break-all" : "normal")};
 `;
 export default Paragraph;
