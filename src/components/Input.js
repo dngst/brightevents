f2e3369834152cdeceed0b6390a483d60a929381
dngst <dngst@protonmail.com>
2022-02-09 14:21:47 +0300

    Deliver components from single file

---
+++ src/components/Input.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Input = styled.input.attrs((props) => ({
+  type: "text",
+}))`
+  border: none;
+  border-bottom: 1px solid
+    ${(props) => (props.transparent ? "props.theme.white" : props.theme.brown)};
+  display: block;
+  height: 2rem;
+  width: 100%;
+  margin: 0 0 1em;
+  text-transform: capitalize;
+  box-sizing: border-box;
+  ::placeholder {
+    color: ${(props) => props.theme.brown};
+  }
+  &:focus {
+    outline: none;
+    border-color: ${(props) => props.theme.white};
+  }
+`;
+
+export const PasswordInput = styled(Input).attrs({
+  type: "password",
+})``;
+
+export const TextArea = styled.textarea`
+  border: none;
+  border-bottom: 1px solid ${(props) => props.theme.brown};
+  height: 6rem;
+  width: 100%;
+  margin: 0 0 1em;
+  text-transform: capitalize;
+  box-sizing: border-box;
+  overflow: auto;
+  resize: none;
+  ::placeholder {
+    color: ${(props) => props.theme.brown};
+  }
+  &:focus {
+    outline: none;
+    border-color: ${(props) => props.theme.white};
+  }
+`;
+
+export const TimePicker = styled(Input).attrs({
+  type: "time",
+})``;
+
+export const DatePicker = styled(Input).attrs({
+  type: "date",
+})``;
+
+export default Input;
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,10 +1,93 @@
 import React, { useState } from "react";
-import Nav from "./Nav";
-import ActiveLink from "./ActiveLink";
-import Logo from "./Logo";
-import Menu from "./Menu";
-import Hamburger from "./Hamburger";
+import { Link, useRoute } from "wouter";
+import styled from "styled-components";
 import Icon from "./icon.png";
+
+const Nav = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background: ${(props) => props.theme.white};
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  .active {
+    text-underline-position: under;
+    text-decoration: underline;
+    text-underline-offset: 0.5em;
+    text-decoration-color: ${(props) => props.theme.base};
+  }
+  @media (max-width: 768px) {
+    border-bottom: 1px solid ${(props) => props.theme.navBorder};
+  }
+`;
+
+const Logo = styled.span`
+  padding: 1rem 0;
+  cursor: pointer;
+  img {
+    height: 2rem;
+    width: 2rem;
+  }
+`;
+
+const ActiveLink = (props) => {
+  const [isActive] = useRoute(props.href);
+  return (
+    <Link {...props}>
+      <a className={isActive ? "active" : ""} href={isActive}>
+        {props.children}
+      </a>
+    </Link>
+  );
+};
+
+const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background: ${(props) => props.theme.base};
+    margin-bottom: 4px;
+    border-radius: 0;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  a {
+    padding: 1rem 2rem;
+    cursor: pointer;
+    text-align: right;
+    text-transform: capitalize;
+    color: #000000;
+    transition: all 0.3s ease-in;
+    font-size: 0.9rem;
+    &:hover {
+      text-underline-position: under;
+      text-decoration: underline;
+      text-underline-offset: 0.5em;
+      text-decoration-color: ${(props) => props.theme.base};
+    }
+  }
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+    width: 100%;
+  }
+`;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
