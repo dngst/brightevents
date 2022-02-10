42f62f3ed226e030157df6befb62a0e7668bf034
dngst <dngst@protonmail.com>
2022-02-10 22:31:33 +0300

    Add tests

---
+++ src/__tests__/Button.test.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import Button from "components/Button";
+
+it("works", () => {
+  const { container } = render(<Button />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+it("applies default styles", () => {
+  const { container } = render(<Button />);
+  expect(container.firstChild).toHaveStyleRule("text-transform", "uppercase");
+  expect(container.firstChild).toHaveStyleRule("border-radius", "0");
+  expect(container.firstChild).toHaveStyleRule("font-size", "0.9em");
+  expect(container.firstChild).toHaveStyleRule("font-weight", "bold");
+});
