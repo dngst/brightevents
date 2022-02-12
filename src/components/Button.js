442029bfd70d88fb1daebe491ff190fa287ce669
dngst <dngst@protonmail.com>
2022-02-12 18:48:16 +0300

    Adjust margin for standalone button

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ const Button = styled.button`
     props.primary ? props.theme.base : props.theme.white};
   border: 1px solid ${(props) => props.theme.base};
   border-radius: 0;
-  margin: 2% auto;
+  margin: ${(props) => (props.max ? "auto" : "2% auto")};
   padding: 0.7em 2em;
   font-size: 0.9em;
   text-transform: uppercase;
