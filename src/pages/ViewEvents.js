4eea74ebd94626a877c5435fb39c3a8310d908df
dngst <dngst@protonmail.com>
2022-02-13 22:45:15 +0300

    Only show the first two lines of an events description

--- src/pages/ViewEvents.js
+++ src/pages/ViewEvents.js
@@ -1,38 +1,42 @@
-import React, { Suspense } from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "wouter";
+import LinesEllipsis from "react-lines-ellipsis";
 import EventsContainer from "components/container/EventsContainer";
-const Title = React.lazy(() => import("components/Title"));
-const Paragraph = React.lazy(() => import("components/Paragraph"));
-const Input = React.lazy(() => import("components/Input"));
-const Card = React.lazy(() => import("components/Card"));
+import Title from "components/Title";
+import Paragraph from "components/Paragraph";
+import Card from "components/Card";
+import Input from "components/Input";
 
 const ViewEvents = () => {
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <EventsContainer>
-        <Helmet>
-          <title>Events | Bright Events</title>
-        </Helmet>
-        <Title>browse events</Title>
-        <Input
-          placeholder="search events by title, location, category"
-          transparent
+    <EventsContainer>
+      <Helmet>
+        <title>Events | Bright Events</title>
+      </Helmet>
+      <Title>browse events</Title>
+      <Input
+        placeholder="search events by title, location, category"
+        transparent
+      />
+      <Card>
+        <Paragraph className="event-title">
+          <span>1 feb</span> <Link to="event">breaking the fast</Link>
+        </Paragraph>
+        <LinesEllipsis
+          maxLine="2"
+          ellipsis="..."
+          text="Deserunt voluptate pariatur tempor et eiusmod ex tempor mollit
+            dolore voluptate quis veniam non in ullamco consequat consectetur
+            dolor labore adipisicing culpa ad do excepteur ut exercitation ut
+            aliquip cupidatat voluptate labore."
+          trimRight
+          basedOn="letters"
         />
-        <Card>
-          <Paragraph className="event-title">
-            <span>1 feb</span> <Link to="event">breaking the fast</Link>
-          </Paragraph>
-          <Paragraph>
-            Deserunt voluptate pariatur tempor et eiusmod ex tempor mollit
-            dolore voluptate...
-          </Paragraph>
-          <Paragraph className="event-cat">
-            <Link to="#">#food</Link>
-          </Paragraph>
-        </Card>
-      </EventsContainer>
-    </Suspense>
+        <Paragraph className="event-cat">
+          <Link to="#">#food</Link>
+        </Paragraph>
+      </Card>
+    </EventsContainer>
   );
 };
 
