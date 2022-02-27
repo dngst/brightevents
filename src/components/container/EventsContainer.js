b492c61fb189f11c54722fdf2a301ccb6a7d5733
dngst <dngst@protonmail.com>
2022-02-27 14:17:47 +0300

    Add line height

--- src/components/container/EventsContainer.js
+++ src/components/container/EventsContainer.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Container from "./Container";
 
 const EventsContainer = styled(Container)`
+  line-height: 1.5;
   width: 35%;
   @media (max-width: 768px) {
     width: 100% !important;
