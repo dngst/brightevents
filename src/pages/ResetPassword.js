56e75965a517692ed4d1531cc10a324aeaa88ffb
dngst <dngst@protonmail.com>
2022-03-27 13:03:37 +0300

    Add link to register page

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react";
+import { Link } from "wouter";
 import { Helmet } from "react-helmet";
+import Paragraph from "components/Paragraph";
 import { EmailInput } from "components/Input";
 const FormContainer = React.lazy(() =>
   import("components/container/FormContainer")
@@ -17,6 +19,9 @@ const ResetPassword = () => {
         <Title>reset password</Title>
         <EmailInput placeholder="email" />
         <Button primary>send link</Button>
+        <Paragraph capitalize>
+          <Link to="/signup">create a new account</Link>
+        </Paragraph>
       </FormContainer>
     </Suspense>
   );
