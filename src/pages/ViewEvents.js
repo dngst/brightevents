b25932a153411d9a16dddea62bc62db2aa2880ee
dngst <dngst@protonmail.com>
2022-03-27 12:42:19 +0300

    Remove react-lines-ellipsis

--- src/pages/ViewEvents.js
+++ src/pages/ViewEvents.js
@@ -1,10 +1,8 @@
 import { Helmet } from "react-helmet";
 import { Link } from "wouter";
-import LinesEllipsis from "react-lines-ellipsis";
 import Container from "components/container/Container";
 import Title from "components/Title";
 import Paragraph from "components/Paragraph";
-import Card from "components/Card";
 import Input from "components/Input";
 
 const ViewEvents = () => {
